1b181d717b8d414e0c462b7f0da712f0313e8a18
baristunar <tunarbaris7@gmail.com>
2021-02-20 12:08:40 +0300

    admin panel created

--- src/components/AdminPanel/AdminPanel.vue
+++ src/components/AdminPanel/AdminPanel.vue
@@ -1,9 +1,71 @@
 <template>
-  <div>admsaaaaaaaaaaaaaaain</div>
+  <v-row>
+    <v-col cols="12">
+      <v-card class="pa-5">
+        <v-toolbar flat color="primary" dark>
+          <!-- Menu -->
+
+          <v-toolbar-title>Admin Paneli</v-toolbar-title>
+        </v-toolbar>
+
+        <v-tabs vertical>
+          <v-tab v-for="item in adminMenu" :key="item">
+            <v-icon left> {{ item.icon }}</v-icon>
+            <span> {{ item.title }}</span>
+          </v-tab>
+
+          <!-- Slider Content -->
+          <v-tab-item>
+            <v-card flat>
+              <v-card-text>
+                <!--   <app-user-information /> -->
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+
+          <v-tab-item>
+            <v-card flat>
+              <v-card-text>
+                <app-create-trademark />
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+
+          <v-tab-item>
+            <v-card flat>
+              <v-card-text>
+                <!--  <appPasswordChange /> -->
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {};
+import appCreateTrademark from "@/components/AdminPanel/appCreateTrademark";
+
+export default {
+  components: {
+    appCreateTrademark,
+  },
+  data() {
+    return {
+      adminMenu: [
+        {
+          title: "Kullanıcı İşlemleri",
+          icon: "mdi-account",
+        },
+        {
+          title: "Marka Oluştur",
+          icon: "mdi-trademark",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/AdminPanel/appCreateTrademark.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row class="d-flex justify-center">
+    <v-col cols="6">
+      <v-card class="pa-5">
+        <v-form>
+          <v-text-field label="Marka Adı"></v-text-field>
+          <v-text-field label="Marka Logosu"> </v-text-field>
+          <v-btn class="info float-right mt-8"> Oluştur</v-btn>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
